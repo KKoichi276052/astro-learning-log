fcac9e0b3c15097ac63a3c0b2a76d2272dd7f1c0
KK <k.koichi276052@gmail.com>
2024-04-16 13:09:58 +0900

    fcc-astro finish crash course

--- fcc-astro-crash-course/astro.config.mjs
+++ fcc-astro-crash-course/astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({ integrations: [UnoCSS({ injectReset: true })] });
+export default defineConfig({
+  integrations: [UnoCSS({
+    injectReset: true
+  }), mdx()]
+});
